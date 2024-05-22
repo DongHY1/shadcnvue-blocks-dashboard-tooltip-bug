26e2f6b3e564e91393142da05fab3e8510b2d620
DongHY1 <hi@daviddong.me>
2024-05-22 14:34:45 +0800

    feat: init

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,9 @@
 <script setup lang="ts">
 import { VueQueryDevtools } from '@tanstack/vue-query-devtools'
-import { Button } from '@/components/ui/button'
 </script>
 
 <template>
-  <div class="w-full h-screen  flex justify-center items-center flex-col gap-y-4">
-    <div>My Vue Starter</div>
-    <div class="flex gap-x-4">
-      <Button>
-        <RouterLink to="/">
-          Go to Home
-        </RouterLink>
-      </Button>
-      <Button>
-        <RouterLink to="/about">
-          Go to About
-        </RouterLink>
-      </Button>
-    </div>
+  <div>
     <RouterView />
     <VueQueryDevtools position="right" />
   </div>
